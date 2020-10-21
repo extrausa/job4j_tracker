24a52286329c8afb8dd3dce97a1e200054e9daa7
extrausa <d.kirenkov@gmail.com>
2020-10-21 15:46:06 +0300

    4. Переопределение [#363144]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,7 +3,8 @@ package ru.job4j.inheritance;
 public class JSONReport extends TextReport {
 
     public String generate(String name, String body) {
-        return  name + " : name"+"," + System.lineSeparator() + body + " : body";
+//        return  name + " : name"+"," + System.lineSeparator() + body + " : body";
+        return String.format("{%n\t\"name\":%s, %n\t\"body\":%s%n}", name, body);
     }
 
 
