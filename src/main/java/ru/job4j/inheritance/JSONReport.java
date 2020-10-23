a4772a962cb501df61b0d88ac693dcb051355f2a
extrausa <d.kirenkov@gmail.com>
2020-10-23 11:11:35 +0300

    5. Аннотация @Override [#363145]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,6 +2,7 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
+    @Override
     public String generate(String name, String body) {
 //        return  name + " : name"+"," + System.lineSeparator() + body + " : body";
         return String.format("{%n\t\"name\":%s, %n\t\"body\":%s%n}", name, body);
