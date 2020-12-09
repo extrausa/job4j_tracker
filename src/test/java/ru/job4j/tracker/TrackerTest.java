521972236d49761d13098c8eed0bac6740043ea1
extrausa <d.kirenkov@gmail.com>
2020-12-09 14:08:19 +0300

    0. Checkstyle с Maven [#363044]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,17 +1,65 @@
 package ru.job4j.tracker;
-
+//0. Сортировка [#363056]
 import org.junit.Test;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.*;
 
 public class TrackerTest {
+
     @Test
-    public void whenAddNewItemThenTrackerHasSameItem() {
+    public void whenReplace() {
         Tracker tracker = new Tracker();
-        Item item = new Item();
-        item.setName("test1");
-        tracker.add(item);
-        Item result = tracker.findById(item.getId());
-        assertThat(result.getName(), is(item.getName()));
+        Item bug = new Item();
+        bug.setName("Bug");
+        tracker.add(bug);
+        int id = bug.getId();
+        Item bugWithDesc = new Item();
+        bugWithDesc.setName("Bug with description");
+        tracker.replace(id, bugWithDesc);
+        assertThat(tracker.findById(id).getName(), is("Bug with description"));
     }
+
+    @Test
+    public void whenDelete() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item();
+        bug.setName("Bug");
+        tracker.add(bug);
+        int id = bug.getId();
+        tracker.delete(id);
+        assertThat(tracker.findById(id), is(nullValue()));
+    }
+
+    @Test
+    public void whenIncrease() {
+        List<Item> items = new ArrayList<>();
+        Item one = new Item();
+        one.setName("Bob");
+        one.setId(1);
+        items.add(one);
+        Item two = new Item();
+        two.setName("Jack");
+        two.setId(2);
+        items.add(two);
+        Comparator name = new ItemComparatorIncrease();
+        Collections.sort(items, name);
+        List<Item> items1 = new ArrayList<>();
+        Item two1 = new Item();
+        two1.setName("Jack");
+        two1.setId(2);
+        items1.add(two1);
+        Item one1 = new Item();
+        one1.setName("Bob");
+        one1.setId(1);
+        items1.add(one1);
+        assertThat(items, is(items1));
+    }
+
 }
